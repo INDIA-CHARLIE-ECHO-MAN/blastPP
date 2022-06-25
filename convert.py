355638dbdedea1c0d4e1825224ed4a8389281d6e
INDIA-CHARLIE-ECHO-MAN <43439611+INDIA-CHARLIE-ECHO-MAN@users.noreply.github.com>
2022-06-26 01:32:36 +1000

    converter done, sussing out blasting

--- convert.py
+++ convert.py
@@ -1,27 +1,36 @@
+import json
+from Bio.Blast import NCBIWWW
+
 source = "UP000008783_418459.fasta"
+jsonFile = "genes.json"
 
 def convert():
+    geneset = []
 
-    geneset = {}
+    # produce list first
+    with open(source, "r") as fastaFile:
+        seq = ""
 
-    # make json file
-    with open('genes.json', 'w') as jsonFile:
+        for line in fastaFile:
+            if (line[:4] == ">tr|"):
+                seq = seq.strip()
+                geneset.append(seq)
+                seq = ""
+            seq = seq + line
+        geneset.append(seq)
+        seq = seq.strip()
 
-        with open(source) as fastaFile:
-            for line in fastaFile:
-                if (line[:4] == ">tr|"):
-                    
+    geneset.remove("")
 
+    with open(jsonFile, "w") as jFile:
+        jFile.write(json.dumps(geneset))
 
-    
-
-
-
-
+def blaster():
+    print("hi")
 
 
 def main():
-    
+    convert()
 
 if __name__ == "__main__":
     main()
